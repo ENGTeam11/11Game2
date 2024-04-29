bb5b0db85a313bf34b7c27f8e41f3431bcfe416c
NewLifeStyle101 <damian.myszko@gmail.com>
2024-04-30 00:40:19 +0100

    controller for the objects

---
+++ core/src/eng1/model/views/minigames/minigames_components/ObstacleSpawner.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ObstacleSpawner {
+    private ArrayList<Obstacle> obstacles;
+
+    public ObstacleSpawner(){
+
+    }
+
+    public ArrayList<Obstacle> initObstacles(){
+        ArrayList<Obstacle> obstaclesInit = new ArrayList<Obstacle>();
+        for(int i =0; i < 10;i++){
+            Obstacle obstacle = new Obstacle()
+            obstaclesInit.add()
+        }
+    }
+
+    public void Update(){
+
+    }
+}
