d59aa49bc04ab1c06e1907b8820b7021e3d96dde
NewLifeStyle101 <damian.myszko@gmail.com>
2024-05-02 14:16:36 +0100

    Now stores an angle if it is given

--- core/src/minigames/minigames_components/Obstacle.java
+++ core/src/minigames/minigames_components/Obstacle.java
@@ -8,29 +8,39 @@ public class Obstacle {
     private Vector2 obstaclePos;
     private Texture obstacleTexture;
     private float speed = 2;
-    private float gravity = 0.4
+    private double gravity = 0.4;
     private CircleBounds obstacleBounds;
     private boolean draw = true;
     private Vector2 moveTrajectory;
+    private int angle =-1;
 
     public Obstacle(Texture inTexture, Vector2 inPosition,float radius){
+        obstaclePos = inPosition;
+        obstacleTexture = inTexture;
+        obstacleBounds = new CircleBounds(obstaclePos, radius);
+     }
+
+    public Obstacle(Texture inTexture, Vector2 inPosition,float radius,int inAngle){
        obstaclePos = inPosition;
        obstacleTexture = inTexture;
+       angle = inAngle;
        obstacleBounds = new CircleBounds(obstaclePos, radius);
     }
 
     public void Update(float delta){
-
+        if(angle >= 0){
+            CalcTrajectory(delta);
+        }
     }
 
-    public void CalcTrajectory(float delta, float angle){
-        Vector2 xVel = (speed*delta) * Math.cos(angle);
-        Vector2 yVel = (speed*delta)*Math.sin(angle) - (1/2)*gravity*delta*delta
-        moveTrajectory = new Vector2(xVel,yVel);
+    public void CalcTrajectory(float delta){
+        double xVel = (speed*delta) * Math.cos(angle);
+        double yVel = (speed*delta)*Math.sin(angle) - (1/2)*gravity*delta*delta;
+        moveTrajectory = new Vector2((float)xVel,(float)yVel);
     }
 
     public void Move(){
-        obstaclePos = obstaclePos - moveTrajectory;
+        obstaclePos = new Vector2(obstaclePos.x - moveTrajectory.x,obstaclePos.y - moveTrajectory.y);
         obstacleBounds.setPosition(obstaclePos);
     }
 
