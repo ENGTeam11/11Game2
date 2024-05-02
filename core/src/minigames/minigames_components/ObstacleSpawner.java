828fb5502f3d5cf0d980db782afd8e400eb3e158
NewLifeStyle101 <85872776+NewLifeStyle101@users.noreply.github.com>
2024-05-02 11:13:54 +0100

    Improved spawning and accessor to the list of obstacles

--- core/src/minigames/minigames_components/ObstacleSpawner.java
+++ core/src/minigames/minigames_components/ObstacleSpawner.java
@@ -1,24 +1,36 @@
 package minigames.minigames_components;
 
 import java.util.ArrayList;
+import java.util;
+import com.badlogic.gdx.Gdx;
 
 public class ObstacleSpawner {
     private final int INIT_OBSTACLES = 10;
     private ArrayList<Obstacle> obstacles;
+    private Texture obstacleTexture;
 
-    public ObstacleSpawner(){
-
+    public ObstacleSpawner(Texture inObstacleTexture){
+        obstacleTexture = inObstacleTexture;
     }
 
     public void InitFoodNinjaObstacles(){
+        Random generator = new Random();
         ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();
         for(int i =0; i < INIT_OBSTACLES;i++){
-            Obstacle obstacle = new Obstacle();
+            int X = generator.nextInt(Gdx.graphics.getWidth())
+            Obstacle obstacle = new Obstacle(obstacleTexture,new Vector2(X,0),5);
             obstacles.add();
         }
     }
 
-    public void Update(){
+    public void Draw(SpriteBatch spriteBatch){
+        for(Obstacle obstacle : obstacles){
+            if(obstacle.getDraw()):
+                obstacle.Draw(spriteBatch);
+        }
+    }
 
+    public ArrayList<Obstacle> getObstacles(){
+        return obstacles;
     }
 }
