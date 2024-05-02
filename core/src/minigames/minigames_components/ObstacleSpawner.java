c9a88c5a66131efc8d8b289be6e8026901327d69
NewLifeStyle101 <damian.myszko@gmail.com>
2024-05-02 14:17:05 +0100

    generates an angle and passes it to the obstacle object

--- core/src/minigames/minigames_components/ObstacleSpawner.java
+++ core/src/minigames/minigames_components/ObstacleSpawner.java
@@ -1,8 +1,12 @@
 package minigames.minigames_components;
 
 import java.util.ArrayList;
-import java.util;
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class ObstacleSpawner {
     private final int INIT_OBSTACLES = 10;
@@ -17,8 +21,9 @@ public class ObstacleSpawner {
         Random generator = new Random();
         ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();
         for(int i =0; i < INIT_OBSTACLES;i++){
-            int X = generator.nextInt(Gdx.graphics.getWidth())
-            Obstacle obstacle = new Obstacle(obstacleTexture,new Vector2(X,0),5);
+            int X = generator.nextInt(Gdx.graphics.getWidth());
+            int angle = generator.nextInt((90-25)+1)+25;
+            Obstacle obstacle = new Obstacle(obstacleTexture,new Vector2(X,0),5,angle);
             obstacles.add(obstacle);
         }
     }
