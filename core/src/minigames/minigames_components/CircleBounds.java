7d29ae9ea8f2cd53f68b3f46b09a2fe21a28408d
NewLifeStyle101 <damian.myszko@gmail.com>
2024-05-02 14:15:09 +0100

    Equation for contains slightly changed

--- core/src/minigames/minigames_components/CircleBounds.java
+++ core/src/minigames/minigames_components/CircleBounds.java
@@ -15,7 +15,7 @@ public class CircleBounds {
         float xDiff = circle1.getPosition().x - circle2.getPosition().x;
         float yDiff = circle1.getPosition().y - circle2.getPosition().y;
 
-        return Math.pow(xDiff,2) + Math.pow(yDiff,2) <= Math.pow(circle1.getRadius(),2);
+        return Math.sqrt(Math.pow(xDiff,2) + Math.pow(yDiff,2)) <= circle1.getRadius();
     }
 
     public Vector2 getPosition(){
