39103b6f4ff5ea49d1d7b590278c33e6d006d972
NewLifeStyle101 <damian.myszko@gmail.com>
2024-05-02 14:13:53 +0100

    Now stores the heslington hussle class and goes back to the game screen when space is pressed

--- core/src/minigames/FoodNinja.java
+++ core/src/minigames/FoodNinja.java
@@ -5,9 +5,13 @@ import java.time.Instant;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.eng1.game.HeslingtonHustle;
+import com.eng1.game.MenuState;
 import com.eng1.game.Play;
 
 import minigames.minigames_components.MinigameState;
@@ -15,18 +19,21 @@ import minigames.minigames_components.Mouse;
 import minigames.minigames_components.ObstacleSpawner;
 
 public class FoodNinja implements Screen {
-    private Play gameState;
+    //holds the parent so it can switch back to the game screen
+    private HeslingtonHustle parent;
+    //players mouse
     private Mouse mouse;
+    //responsible for the game state
     private MinigameState miniGState;
     private ObstacleSpawner obstaclesManager;
     private Instant startTime;
     private Stage canvas;
     private final int GAME_LENGTH_SECONDS = 30;
 
-    public FoodNinja(Play game){
-        gameState = game;
+    public FoodNinja(HeslingtonHustle inParent){
+        parent = inParent;
         miniGState = MinigameState.WAIT;
-        obstaclesManager = new ObstacleSpawner();
+        obstaclesManager = new ObstacleSpawner(Gdx.);
         mouse = new Mouse();
         canvas = new Stage(new StretchViewport(800, 600));
     }
@@ -39,21 +46,27 @@ public class FoodNinja implements Screen {
     @Override
     public void render(float delta) {
         canvas.getBatch().begin();
+        //Gets the current time from the system clock
         Instant gameTime = Instant.now();
+        //This if statement takes care of the state before the game begins where the player has to click space to begin
         if(miniGState == MinigameState.WAIT){
             if(Gdx.input.isKeyPressed(62)){
-                miniGState = MinigameState.START;
+                miniGState = MinigameState.END;
                 startTime = Instant.now();
             }
         }
+        //This if statement takes care of mini game logic 
         else if(miniGState == MinigameState.START){
+            /*checks the difference between the start time and end time of the game in seconds since 1970 and
+            checks if the difference is longer than the set game length in seconds*/
             if(gameTime.getEpochSecond() - startTime.getEpochSecond() >= GAME_LENGTH_SECONDS){
                 miniGState = MinigameState.END;
             }
             mouse.Update(delta);
         }
+        //This if statement is responsible for handling the end of the game 
         else if(miniGState == MinigameState.END){
-
+            parent.changeScreen(MenuState.APPLICATION);
         }
         canvas.getBatch().end();
     }
