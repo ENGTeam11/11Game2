0179202aee16e9081f3cace18b4595d93b71fb12
NewLifeStyle101 <damian.myszko@gmail.com>
2024-05-06 21:03:09 +0100

    bound position updated relating to the drawing axis

--- core/src/minigames/minigames_components/Mouse.java
+++ core/src/minigames/minigames_components/Mouse.java
@@ -1,22 +1,25 @@
 package minigames.minigames_components;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Mouse {
     private Vector2 mousePos;
-    private CircleBounds circleBounds;
+    private Circle circleBounds;
     public Mouse(){
-        mousePos = new Vector2();
-        circleBounds = new CircleBounds(mousePos, 5);
+        mousePos = new Vector2();  
+        circleBounds = new Circle(mousePos, 16);
     }
 
     public void Update(float delta){
         mousePos.set(Gdx.input.getX(),Gdx.input.getY());
-        circleBounds.setPosition(mousePos);
+        float circlePosY = Gdx.graphics.getBackBufferHeight()-mousePos.y;
+        Vector2 circlePosition = new Vector2(mousePos.x,circlePosY);
+        circleBounds.setPosition(circlePosition);
     }
 
-    public CircleBounds getCircleBounds(){
+    public Circle getCircleBounds(){
         return circleBounds;
     }
     
