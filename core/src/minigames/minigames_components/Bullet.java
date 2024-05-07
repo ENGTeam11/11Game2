b916dc9bdc9addf1243b20ac28a695dbbee5486e
NewLifeStyle101 <damian.myszko@gmail.com>
2024-05-07 23:14:29 +0100

    added bounds and checks if the button is within screen

--- core/src/minigames/minigames_components/Bullet.java
+++ core/src/minigames/minigames_components/Bullet.java
@@ -8,28 +8,30 @@ import com.badlogic.gdx.math.Vector2;
 public class Bullet extends Obstacle{
     private Texture bulletTexture;
 
-    public Bullet(Texture inBulletTexture, Vector2 inBulletPositions){
+    public Bullet(Texture inBulletTexture, Vector2 inBulletPositions,Circle inBulletBounds){
         super();
         bulletTexture = inBulletTexture;
         obstaclePos = inBulletPositions;
+        obstacleBounds = inBulletBounds;
         speed = 40;
-        gravity = 0;
     }
 
     public void update(float deltaTime){
+        checkObstacleIsWithinScreen();
         move(deltaTime);
         Circle newBulletBoundsPos = new Circle(obstaclePos.x + bulletTexture.getWidth()/2,obstaclePos.y + bulletTexture.getHeight()/2 ,bulletTexture.getWidth()/2);
         obstacleBounds = newBulletBoundsPos;
     }
 
-    public void move(float deltaTime){
+    private void move(float deltaTime){
         moveTrajectory = getNormalizedVectorDirection(obstaclePos, moveTrajectory);
         Vector2 moveBy = moveTrajectory.add(speed, speed);
         moveBy = moveBy.scl(deltaTime);
         obstaclePos = obstaclePos.add(moveBy);
     }
 
-    public Vector2 getNormalizedVectorDirection(Vector2 playerPos, Vector2 mousePos){
+
+    private Vector2 getNormalizedVectorDirection(Vector2 playerPos, Vector2 mousePos){
         float directionX = mousePos.x - playerPos.x;
         float directionY = mousePos.y - playerPos.y;
         Vector2 directionVector = new Vector2(directionX,directionY);
