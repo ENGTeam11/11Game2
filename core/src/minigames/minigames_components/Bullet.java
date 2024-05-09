d748bcc1aa651a538d799c3085d412aba51d7fb1
NewLifeStyle101 <damian.myszko@gmail.com>
2024-05-09 04:51:24 +0100

    implemented the necessary logic

--- core/src/minigames/minigames_components/Bullet.java
+++ core/src/minigames/minigames_components/Bullet.java
@@ -1,9 +1,11 @@
 package minigames.minigames_components;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 
 public class Bullet extends Obstacle{
@@ -14,31 +16,28 @@ public class Bullet extends Obstacle{
         bulletTexture = inBulletTexture;
         obstaclePos = inBulletPositions;
         obstacleBounds = inBulletBounds;
-        speed = 40;
+        speed = 200;
         getBulletTextureRegion();
     }
     private void getBulletTextureRegion(){
-        obstacleTexture = new TextureRegion(bulletTexture,0,14*16,16,16);
+        obstacleTexture = new TextureRegion(bulletTexture,0,13*16,16,16);
     }
+
+    @Override
     public void update(float deltaTime){
         checkObstacleIsWithinScreen();
         move(deltaTime);
-        Circle newBulletBoundsPos = new Circle(obstaclePos.x + bulletTexture.getWidth()/2,obstaclePos.y + bulletTexture.getHeight()/2 ,bulletTexture.getWidth()/2);
+        Circle newBulletBoundsPos = new Circle(obstaclePos.x + obstacleTexture.getRegionWidth()/2,obstaclePos.y + obstacleTexture.getRegionHeight()/2 ,obstacleTexture.getRegionWidth()/2);
         obstacleBounds = newBulletBoundsPos;
     }
 
     private void move(float deltaTime){
-        moveTrajectory = getNormalizedVectorDirection(obstaclePos, moveTrajectory);
-        Vector2 moveBy = moveTrajectory.add(speed, speed);
-        moveBy = moveBy.scl(deltaTime);
-        obstaclePos = obstaclePos.add(moveBy);
+        Vector2 moveBy = new Vector2(moveTrajectory.x *(speed * deltaTime), moveTrajectory.y * (speed * deltaTime));
+        obstaclePos = new Vector2(obstaclePos.x + moveBy.x , obstaclePos.y + moveBy.y);
     }
 
-    private Vector2 getNormalizedVectorDirection(Vector2 playerPos, Vector2 mousePos){
-        float directionX = mousePos.x - playerPos.x;
-        float directionY = mousePos.y - playerPos.y;
-        Vector2 directionVector = new Vector2(directionX,directionY);
-        return directionVector.nor();
+    public void setMoveTrajectory(Vector2 value){
+        moveTrajectory = value;
     }
 
 }
