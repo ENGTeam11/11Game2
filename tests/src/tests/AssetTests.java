bee6eb437644a425d4eb0fa1eb7a9a4c72cc54d5
Domdec32 <deciccodominic32@gmail.com>
2024-05-09 10:40:21 +0100

    Testing beginnings

---
+++ tests/src/tests/AssetTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import com.badlogic.gdx.Gdx;
+import eng1.model.views.CharacterScreen;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+    @Test
+    public void testShipAssetExists() {
+        System.out.println(Gdx.files.internal("").exists());
+        assertTrue("The asset for player 1 exists",
+                Gdx.files.internal("playerCharacters/playerCharacter1.png").exists());
+    }
+}
